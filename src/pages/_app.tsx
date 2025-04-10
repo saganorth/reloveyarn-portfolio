9fe8c60bb4c912fe8f2fb6714393a1abe80ae239
saganorth <saga.nord99@hotmail.se>
2025-04-10 21:39:10 +0200

    da

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CartProvider } from '../context/CartContext';
-
+import { CartProvider } from "@/context/CartContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <CartProvider>
-  <Component {...pageProps} />
-</CartProvider>
-);
+    <CartProvider>
+      <Component {...pageProps} />
+      <Analytics />
+    </CartProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
+
 import React from "react";
 export default function Document() {
   return (
@@ -9,7 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
+  
       </body>
     </Html>
   );
